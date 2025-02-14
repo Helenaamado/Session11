38d366fe9b006f89b0697c25c34386e233a7ab5b
helenaamado <helena.aamado@gmail.com>
2025-02-14 12:56:20 +0100

    dictionary

---
+++ dct_example.py
@@ -0,0 +1,27 @@
+d= {} #empty dict
+eng_to_spa = {"one": "uno", "two":"dos", "three":"tres"}
+print(eng_to_spa)
+print(eng_to_spa["three"])
+eng_to_spa.update({"yes": "si", "no" : "no", "am": "soy", "i": "yo", "cuban":"cubano"})
+print(eng_to_spa)
+print(f"i know {len(eng_to_spa)} spanish words")
+sentence= "i am cuban"
+words= sentence.split()
+translation= " "
+for word in words:
+    translation += eng_to_spa[word]+" "
+print(f"{sentence} translate to: {translation}")
+print(list(eng_to_spa.keys()))
+eng_to_spa.pop("no")
+print(eng_to_spa)
+
+
+if "car" in eng_to_spa:
+    print(eng_to_spa["car"])
+else:
+    print("I dont know this word")
+
+print(eng_to_spa.get("car", "sorry dont know"))
+eng_to_spa.setdefault("one", "sorry dont know")
+eng_to_spa.setdefault("monkey", "sorry dont know")
+print(eng_to_spa)
